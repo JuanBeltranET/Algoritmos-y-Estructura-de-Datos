25945e5390d4e92f53be73f615e48ef89947ff69
JuanBeltranET <etjuanmanuelbeltran@gmail.com>
2026-08-24 11:29:33 -0300

    Add files via upload

---
+++ Escritor de Notas Diario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+string ruta = "diario.txt";
+Console.WriteLine("Escribe tu nombre y una frase favorita para guardar en el archivo:");
+Console.WriteLine("Nombre:");
+string nombre = Console.ReadLine();
+Console.WriteLine("Frase Favorita:");
+string frase = Console.ReadLine();
+using (StreamWriter escritor = new StreamWriter(ruta, append: false))
+{
+    escritor.WriteLine($"Nombre: {nombre}");
+    escritor.WriteLine($"Frase Favorita: {frase}");
+}
+using (StreamWriter sw = new StreamWriter(ruta, true))
+
+{
+    sw.WriteLine($"Última actualización: {DateTime.Now}");
+}
+Console.WriteLine("Archivo guardado correctamente.");
